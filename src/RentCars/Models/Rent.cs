3a5cefde4e73874f11cadb11c8c9186b7dca8ea3
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-09-25 17:12:07 -0300

    feat: Add this to the calling of the properties of Rent class

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -13,11 +13,11 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        Vehicle = vehicle;
-        Person = person;
-        DaysRented = daysRented;
-        Status = RentStatus.Confirmed;
-        Vehicle.IsRented = true;
+        this.Vehicle = vehicle;
+        this.Person = person;
+        this.DaysRented = daysRented;
+        this.Status = RentStatus.Confirmed;
+        this.Vehicle.IsRented = true;
         // if (typeof(PhysicalPerson).IsInstanceOfType(person))
         // {
         //     Price = Vehicle.PricePerDay * DaysRented;
@@ -27,22 +27,22 @@ public class Rent
         //     Price = Vehicle.PricePerDay * DaysRented;
         //     Price -= Price * 0.1;
         // }
-        double totalPrice = Vehicle.PricePerDay * DaysRented;
-        Price = typeof(PhysicalPerson).IsInstanceOfType(person) ? totalPrice : totalPrice - totalPrice * 0.1;
-        Person.Debit = Price;
+        double totalPrice = this.Vehicle.PricePerDay * this.DaysRented;
+        this.Price = typeof(PhysicalPerson).IsInstanceOfType(person) ? totalPrice : totalPrice - totalPrice * 0.1;
+        this.Person.Debit = this.Price;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Cancel()
     {
-        Status = RentStatus.Canceled;
-        Vehicle.IsRented = false;
+        this.Status = RentStatus.Canceled;
+        this.Vehicle.IsRented = false;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
     public void Finish()
     {
-        Status = RentStatus.Finished;
-        Vehicle.IsRented = false;
+        this.Status = RentStatus.Finished;
+        this.Vehicle.IsRented = false;
     }
 }
